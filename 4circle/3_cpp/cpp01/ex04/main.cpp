ce9ef30e88fd7b62c1e4c3983583f2f2c0e66cad
highcho <highjcho@gmail.com>
2022-10-07 15:50:45 +0900

    [FIX] CPP01 ex04(multiple replacements, argv parse)

--- 4circle/3_cpp/cpp01/ex04/main.cpp
+++ 4circle/3_cpp/cpp01/ex04/main.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
 #include <fstream>
 
-std::string replace_str(std::string str, std::string original, std::string replace) {
+void replace_str(std::ofstream &outfile, std::string &str, std::string to_find, std::string replace) {
+	std::string search;
 	std::string new_str;
-	size_t n = str.find(original);
+	size_t r_size = replace.size();
+	size_t idx = 0;
+	size_t n;
 
-	if (n != std::string::npos) {
-		new_str = str.substr(0, n);
-		new_str += replace;
-		new_str += str.substr(n + original.size());
-		return new_str;
+	while (str[idx]) {
+		search = str.substr(idx);
+		n = search.find(to_find);
+		if (n != std::string::npos) {
+			new_str = search.substr(0, n);
+			outfile << new_str << replace;
+			idx += r_size + n;
+		}
+		else {
+			outfile << search;
+			break;
+		}
 	}
-	return str;
 }
 
 int main(int ac, char** av) {
-	char* tmp = strdup(av[1]);
-	char* in_name = strdup(tmp);
-	char* out_name = strcat(tmp, ".replace");
-	std::ifstream infile(in_name);
-	std::ofstream outfile(out_name);
+	std::string in_file(av[1]);
+	std::ifstream infile(in_file);
+	std::ofstream outfile(in_file + ".replace");
 	std::string str;
 
 	if (ac != 4) {
-		std::cout << "We need more information!\n";
-		return (0);
+		std::cout << "We need more information! (usage: infile, to_find, replace)\n";
+		exit(EXIT_SUCCESS);
 	}
  	if (infile.is_open() && outfile.is_open()) {
-		while(!infile.eof()) {
-			std::getline(infile, str);
-			str = replace_str(str, av[2], av[3]);
-			outfile << str << "\n";
+		while(std::getline(infile, str)) {
+			replace_str(outfile, str, av[2], av[3]);
+			if (!infile.eof())
+				outfile << "\n";
 		}
 	}
-	else
+	else {
 		std::cout << "File open error\n";
+		exit(EXIT_FAILURE);
+	}
+	infile.close();
+	outfile.close();
+	exit(EXIT_SUCCESS);
 }
