997276a9b97aefd898545fd694bb4f41633bbbd7
highcho <highjcho@gmail.com>
2023-03-16 12:02:36 +0900

    [FIX] fix inception's bug

--- 5circle/2_inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ 5circle/2_inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -12,7 +12,7 @@ service mysql start
 sed -i "s/bind-address/#bind-address/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s/password =/password = $MARIADB_ROOT_PWD/g" /etc/mysql/debian.cnf
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '{$MARIADB_ROOT_PWD}';" | mysql -uroot -p$MARIADB_ROOT_PWD
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';" | mysql -uroot -p$MARIADB_ROOT_PWD
 echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;" | mysql -uroot -p$MARIADB_ROOT_PWD
 echo "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PWD';" | mysql -uroot -p$MARIADB_ROOT_PWD
 echo "CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PWD';" | mysql -uroot -p$MARIADB_ROOT_PWD
--- 5circle/2_inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ 5circle/2_inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -8,13 +8,11 @@ nginx \
 openssl \
 vim
 
-if [ ! -d /etc/ssl ]; then
-	openssl	req -x509 -nodes -days 365 -newkey rsa:2048 \
-			-keyout /etc/ssl/private/hyunjcho.42.fr.pem \
-			-out /etc/ssl/hyunjcho.42.fr.crt \
-			-subj "/C=KR/ST=Seoul/L=Gaepo/O=42Seoul/OU=./CN=hyunjcho.42.fr/emailAddress=hyunjcho@student.42seoul.kr"
-	yes | cp var/www/html/default /etc/nginx/sites-available/default
-fi
+openssl	req -x509 -nodes -days 365 -newkey rsa:2048 \
+		-keyout /etc/ssl/private/server_pkey.pem \
+		-out /etc/ssl/certs/server.crt \
+		-subj "/C=KR/ST=Seoul/L=Gaepo/O=42Seoul/OU=./CN=hyunjcho.42.fr/emailAddress=hyunjcho@student.42seoul.kr"
+yes | cp var/www/html/default /etc/nginx/sites-available/default
 
 echo finish setting nginx
 exec "$@"
